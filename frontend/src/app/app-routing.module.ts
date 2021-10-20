8e630dc723748e84c2802d24da3515b5622c29fa
Gabriele Napoli <gabrielenapoli@MacBook-Pro-di-Gabriele.local>
2021-10-20 17:15:48 +0200

    add routing auth module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { LayoutComponent } from "./layout/layout.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    children: [
+      {
+        path: "auth",
+        loadChildren: () =>
+          import("./auth/auth.module").then((m) => m.AuthModule),
+      },
+      {
+        path: "",
+        component: LayoutComponent,
+        children: [
+          {
+            path: "dashboard",
+            component: DashboardComponent,
+          },
+          {
+            path: "",
+            redirectTo: "dashboard",
+            pathMatch: "full",
+          },
+        ],
+      },
+    ],
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
